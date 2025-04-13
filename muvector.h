a3cc3365ff24638f8a91dd28ea46b60913f4f734
Ayoub Mounim <ayoub.mounim@gmail.com>
2025-04-13 18:46:21 +0200

    muvector allocation implemented

--- muvector.h
+++ muvector.h
@@ -1,23 +1,9 @@
 
 #pragma once
 
-#include <assert.h>
-#include <stdlib.h>
+#include <stddef.h>
 
-#ifndef MUVECTOR_ALLOC
-#include <stdlib.h>
-#define MUVECTOR_ALLOC(size) malloc(size)
-#endif
-
-#ifndef MUVECTOR_REALLOC
-#include <stdlib.h>
-#define MUVECTOR_REALLOC(ptr, size) realloc(ptr, size)
-#endif
-
-#ifndef MUVECTOR_FREE
-#include <stdlib.h>
-#define MUVECTOR_FREE(ptr) free(ptr)
-#endif
+#define MUVECTOR_INLINE static inline
 
 #ifndef MUVECTOR_ASSERT
 #include <assert.h>
@@ -28,29 +14,107 @@
 #define MUVECTOR_DEFAULT_CAPACITY 128
 #endif
 
-#define MUVECTOR_RESET(v)                                                      \
+struct muvector_allocator {
+  void *ctx;
+  void *(*alloc)(void *ctx, size_t const size);
+  void *(*realloc)(void *ctx, void *ptr, size_t const size);
+  void (*free)(void *ctx, void **ptr);
+};
+
+#define MUVECTOR_NOT_IMPLEMENTED(func)                                         \
+  MUVECTOR_ASSERT(func && "function not implemented")
+
+#define muvector_allocator_alloc(allocator, size)                              \
+  (MUVECTOR_NOT_IMPLEMENTED((allocator)->alloc),                               \
+   (allocator)->alloc((allocator)->ctx, size))
+
+#define muvector_allocator_realloc(allocator, ptr, size)                       \
+  (MUVECTOR_NOT_IMPLEMENTED((allocator)->realloc),                             \
+   (allocator)->realloc((allocator)->ctx, ptr, size))
+
+#define muvector_allocator_free(allocator, ptr)                                \
+  (MUVECTOR_NOT_IMPLEMENTED((allocator)->free),                                \
+   (allocator)->free((allocator)->ctx, ptr))
+
+#ifdef MUVECTOR_NO_STDLIB
+// TODO: implement some sort of std allocator in this case.
+static struct muvector_allocator muvector_std_allocator = {0};
+#else
+#include <stdlib.h>
+
+MUVECTOR_INLINE void *muvector_std_alloc(void *ctx, size_t const size) {
+  return malloc(size);
+}
+
+MUVECTOR_INLINE void *muvector_std_realloc(void *ctx, void *ptr,
+                                           size_t const size) {
+  (void)ctx;
+  return realloc(ptr, size);
+}
+
+MUVECTOR_INLINE void muvector_std_free(void *ctx, void **ptr) {
+  (void)ctx;
+  free(*ptr);
+  *ptr = NULL;
+  return;
+}
+
+static struct muvector_allocator muvector_std_allocator = {
+    .ctx = NULL,
+    .alloc = muvector_std_alloc,
+    .realloc = muvector_std_realloc,
+    .free = muvector_std_free,
+};
+
+#endif // MUVECTOR_NO_STDLIB
+
+#define muvector_reset(v)                                                      \
   do {                                                                         \
     (v)->length = 0;                                                           \
   } while (0)
 
-#define MUVECTOR_DESTROY(v)                                                    \
+#define muvector_destroy(v)                                                    \
   do {                                                                         \
-    MUVECTOR_FREE((v)->items);                                                 \
-    (v)->length = 0;                                                           \
-    (v)->capacity = 0;                                                         \
+    if ((v)->allocator) {                                                      \
+      (v)->allocator->free((v)->allocator->ctx, (void **)&((v)->items));       \
+    }                                                                          \
   } while (0)
 
-#define MUVECTOR_APPEND(v, item)                                               \
+#define muvector_append(v, item)                                               \
   do {                                                                         \
     if ((v)->length == (v)->capacity) {                                        \
+      if ((v)->allocator == NULL) {                                            \
+        MUVECTOR_ASSERT(0 && "muvector full and no allocator available");      \
+      }                                                                        \
       if ((v)->capacity == 0)                                                  \
         (v)->capacity = MUVECTOR_DEFAULT_CAPACITY;                             \
       else                                                                     \
         (v)->capacity *= 2;                                                    \
-      (v)->items =                                                             \
-          MUVECTOR_REALLOC((v)->items, (v)->capacity * sizeof(*((v)->items))); \
-      assert((v)->items && "muvector realloc failed");                         \
+      (v)->items = muvector_allocator_realloc(                                 \
+          (v)->allocator, (v)->items, (v)->capacity * sizeof(*((v)->items)));  \
+      MUVECTOR_ASSERT((v)->items && "muvector allocator realloc failed");      \
     }                                                                          \
     (v)->items[(v)->length] = item;                                            \
     (v)->length++;                                                             \
   } while (0)
+
+#define MUVECTOR_DEFINE(type)                                                  \
+  struct muvector_##type {                                                     \
+    type *items;                                                               \
+    size_t length;                                                             \
+    size_t capacity;                                                           \
+    struct muvector_allocator *allocator;                                      \
+  }
+
+#define muvector_init_static(v, type, init_capacity)                           \
+  struct muvector_##type v = {0};                                              \
+  type __darray_buffer_##v[init_capacity];                                     \
+  v.items = __darray_buffer_##v;                                               \
+  v.capacity = init_capacity;                                                  \
+  v.allocator = NULL
+
+#define muvector_init_std(v, type)                                             \
+  struct muvector_##type v = {0};                                              \
+  v.allocator = &muvector_std_allocator
+
+#undef MUVECTOR_INLINE
--- test/vector.c
+++ test/vector.c
@@ -1,27 +1,45 @@
 #include "../muvector.h"
+#include <assert.h>
 #include <complex.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-struct int_vector {
-  int *items;
-  size_t length;
-  size_t capacity;
-};
+MUVECTOR_DEFINE(int);
 
 int test_01(void) {
   int res = EXIT_SUCCESS;
-  struct int_vector v = {0};
-  for (int i = 0; i < 100; i++) {
-    MUVECTOR_APPEND(&v, i);
+#define VECTOR_LENGTH 512
+  muvector_init_static(v, int, VECTOR_LENGTH);
+  for (int i = 0; i < VECTOR_LENGTH; i++) {
+    muvector_append(&v, i);
   }
+  assert(v.length == VECTOR_LENGTH);
   for (int i = 0; i < v.length; i++) {
     if (v.items[i] != i) {
       res = EXIT_FAILURE;
       goto exit;
     }
   }
-
 exit:
+  return res;
+}
+
+int test_02(void) {
+  int res = EXIT_SUCCESS;
+  muvector_init_std(v, int);
+#define VECTOR_LENGTH 512
+  for (int i = 0; i < VECTOR_LENGTH; i++) {
+    muvector_append(&v, i);
+  }
+  assert(v.length == VECTOR_LENGTH);
+  for (int i = 0; i < v.length; i++) {
+    if (v.items[i] != i) {
+      res = EXIT_FAILURE;
+      goto exit;
+    }
+  }
+exit:
+  muvector_destroy(&v);
   return res;
 }
 
@@ -29,6 +47,12 @@ int main(int argc, char *argv[]) {
   printf("\n======================= vector tests ========================\n");
   printf(">>> test_01 ... ");
   if (test_01() == EXIT_SUCCESS) {
+    printf("ok\n");
+  } else {
+    printf("fail\n");
+  }
+  printf(">>> test_02 ... ");
+  if (test_02() == EXIT_SUCCESS) {
     printf("ok\n");
   } else {
     printf("fail\n");
